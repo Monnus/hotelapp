76478e0f5483f0ecae7ea5e9115f6e1c1acd542f
Monnus <maun2velsket@gmail.com>
2022-07-12 12:25:56 -0700

    second commit

---
+++ src/pages/landingPage.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+import "./components/styles/landingpage.css"
+export default function LandingPage() {
+  return (
+    <div  className='Page'>
+        
+        
+        
+        LandingPage
+        
+        
+        </div>
+  )
+}
